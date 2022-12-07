7538ff2f0ff0cb9e93f26552b86e4e46be431e7e
cameb1104 <119345778+cameb1104@users.noreply.github.com>
2022-12-07 13:10:31 -0500

    Update board.py

--- board.py
+++ board.py
@@ -1,39 +1,79 @@
-from letter_state import LetterState
-from typing import List
+from wordle import Wordle
 from colorama import Fore
+from board import Board, Convert
+import random
 
+class LoadData:
 
-class Convert:
-    def color_result(result: List[LetterState]):
+    def load_word_set(path: str):
         """
+        Only keys no values
+        """
+        word_set = set()
+        with open(path, "r") as f:
+            for line in f.readlines():
+                word = line.strip().upper()
+                word_set.add(word)
+        return word_set
+
+
+class Results:
+
+    def display_results(wordle: Wordle):
+        print("\nYour results so far...\n")
+        print(f"you have {wordle.prop_remaining_attempts} attempts remaining.\n")
         
-        """
-        result_with_color = []
-        for letter in result:
-            if letter.is_in_position:
-                color = Fore.GREEN
-            elif letter.is_in_word:
-                color = Fore.YELLOW
-            else: 
-                color = Fore.WHITE
-            colored_letter = color + letter.character + Fore.RESET
-            result_with_color.append(colored_letter)
-        return " ".join(result_with_color)  
-
-class Board:
-    
-    def draw_boarder_around(lines: list[str], size: int = 9, pad: int = 1):
-        """
+        lines = []
         
-        """
-        content_length = size + pad * 2
-        top_border = "┌" + "─" * content_length + "┐"
-        bottom_border = "└" + "─" * content_length + "┘"
-        space = " " * pad
-
-        print(top_border)
-
-        for line in lines:
-            print("│" + space + line + space + "│")
+        for word in wordle.attempts:
+            result = wordle.get_guess(word)
+            colored_result_str = Convert.color_result(result)
+            lines.append(colored_result_str)
         
-        print(bottom_border)
+        for i in range(wordle.prop_remaining_attempts):
+            lines.append(" ".join(["_"] * wordle.MAX_LENGTH))
+
+        Board.draw_boarder_around(lines)
+
+
+class Main(Results):
+
+    def main():
+        """
+        main function
+        """
+        word_set = LoadData.load_word_set("data/wordle_words.txt")
+        secret = random.choice(list(word_set))
+        wordle = Wordle(secret)
+
+        while wordle.prop_can_guess:
+            x = input('\nInput your guess: ')
+
+            if len(x) != wordle.MAX_LENGTH:
+                print(
+                    Fore.RED 
+                    + f"Word must be {wordle.MAX_LENGTH} characters long" 
+                    + Fore.RESET
+                )
+                continue
+
+            if not x in word_set:
+                print(
+                    Fore.RED
+                    + f"{x} is not a valid word!"
+                    + Fore.RESET
+                )
+                continue
+
+            wordle.get_attempt(x)
+            Results.display_results(wordle)
+            
+        if wordle.prop_solved:
+            print(f"You've solved the puzzle with {wordle.prop_remaining_attempts} attempts left!")
+        else:
+            print("You've failed to solve the puzzle")
+            print(f"The secret word was: {wordle.key}")
+
+# End check
+if __name__ == "__main__":
+    Main.main()
